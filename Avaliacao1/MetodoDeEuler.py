474b37ee43a1ce6c036a54ed9a8de3bcd6351a08
Nyon0K <cristianvilela0K@gmail.com>
2022-05-12 22:15:20 -0300

    + metodo de Euler para sistemas de EDOs

--- Avaliacao1/MetodoDeEuler.py
+++ Avaliacao1/MetodoDeEuler.py
@@ -1,20 +1,41 @@
+import math
+
 #função da EDO dada
-def f(x, y):
-    r = y - x
+def fL1(L1, L2, t):
+    r = -0.4427*math.sqrt(L1)+0.23*(4+4*math.sin(t))
     return r
 
-#metodo de Euler
-def euler(x0, y0, h, qPassos):
+def fL2(L1, L2, t):
+    r = -0.4427*math.sqrt(L2)+0.4427*math.sqrt(L1)
+    return r
+
+#metodo de Euler Sistema
+def eulerSistema(t, h, qPassos, L10, L20):
+    L1 = L10
+    L2 = L20
     for i in range(qPassos):
-        y_atual = y0+h*f(x0, y0)
-        y0 = y_atual
-        x0 = x0+h
-    return y_atual
+        kL1 = fL1(L1, L2, t)
+        print('kL1', i, kL1)
+        kL2 = fL2(L1, L2, t)
+        print('kL2', i, kL2)
+        L1_atual = L1+kL1*h
+        L2_atual = L2+kL2*h
+        L1 = L1_atual
+        L2 = L2_atual
+        t += h
+    dados.append(t)
+    dados.append(L1)
+    dados.append(L2)
+    return dados
 
 #Dados do PVI
-intervalo = [0, 1]
-x0, y0 = 0, 2
-qPassos = 4
-h = (intervalo[1]-intervalo[0])/qPassos
-print('Raiz aproximada:', euler(x0, y0, h, qPassos))
+dados = []
+intervalo = [0, 30]
+# x0, y0 = 0, 2
+t, L10, L20 = 0, 0, 0
+qPassos = 2
+# h = (intervalo[1]-intervalo[0])/qPassos
+h = 1
+dados = []
+print('Raiz aproximada:', eulerSistema(t, h, qPassos, L10, L20))
 
---
+++ Avaliacao1/MetodoEulerSistemasEDO.py
@@ -0,0 +1,20 @@
+#função da EDO dada
+def f(x, y):
+    r = y - x
+    return r
+
+#metodo de Euler
+def euler(x0, y0, h, qPassos):
+    for _ in range(qPassos):
+        y_atual = y0+h*f(x0, y0)
+        y0 = y_atual
+        x0 = x0+h
+    return y_atual
+
+#Dados do PVI
+intervalo = [0, 1]
+x0, y0 = 0, 2
+qPassos = 4
+h = (intervalo[1]-intervalo[0])/qPassos
+print('Raiz aproximada:', euler(x0, y0, h, qPassos))
+
